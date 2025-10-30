91a6d0fb5c716e0c7c577a96b10f33540d40f5bd
GabrielBenini <gabriel.benini2@gmail.com>
2025-10-30 20:51:09 -0300

    first commit

---
+++ src/test/java/com/gbenini/MovieBox/MovieBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbenini.MovieBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
